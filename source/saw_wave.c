57f674494542916cf17db99465678c3701ee7128
Quentin <quentin.sandoz@epfl.ch>
2023-12-15 13:32:42 +0100

    ebauche sawFill

--- source/saw_wave.c
+++ source/saw_wave.c
@@ -1,2 +1,22 @@
 #include "sound.h"
+#include "saw_wave.h"
 
+void SawFill(void* array, int frequency, int* length){
+
+	int periodSampleNumber = SAMPLERATE / frequency;
+
+	*length = periodSampleNumber;
+
+	//int slope = (valMax - valMin) / periodSampleNumber;
+
+	//int currentValue = valMin;
+
+	for(int i = 0 ; i < periodSampleNumber ; i++){
+
+		//array[i] = currentValue;
+
+		//currentValue += slope;
+	}
+
+
+}
--- source/saw_wave.h
+++ source/saw_wave.h
@@ -1,1 +1,3 @@
+
+//Fills the array passed as argument with samples and then writes it's length in the int passed as argument
 void SawFill(void* array, int frequency, int* length);
