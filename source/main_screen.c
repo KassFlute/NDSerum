32bf13c35306a4049cd185292da5259e8e0752c7
Quentin <quentin.sandoz@epfl.ch>
2024-01-03 19:05:09 +0100

    la saw s'affiche presque comme prévu

--- source/main_screen.c
+++ source/main_screen.c
@@ -6,52 +6,61 @@
 
 void InitMainScreen(){
 
+	REG_DISPCNT = MODE_5_2D | DISPLAY_BG2_ACTIVE | DISPLAY_BG3_ACTIVE;
 
-
-	REG_DISPCNT = MODE_5_2D | DISPLAY_BG2_ACTIVE; //DISPLAY_BG0_ACTIVE
 
 	VRAM_A_CR = VRAM_ENABLE | VRAM_A_MAIN_BG;
+	VRAM_B_CR = VRAM_ENABLE | VRAM_B_MAIN_BG;
 
-	BGCTRL[2] = BG_BMP_BASE(0) | BgSize_B16_256x256;
+	BGCTRL[3] = BG_BMP_BASE(0) | BgSize_B8_256x256;
+
+
+	REG_BG3PA = 256;
+	REG_BG3PC = 0;
+	REG_BG3PB = 0;
+	REG_BG3PD = 256;
+
+	swiCopy(BGMainBitmap,BG_BMP_RAM(0),BGMainBitmapLen/2);
+
+	swiCopy(BGMainPal,BG_PALETTE,BGMainPalLen/2);
+
+	BGCTRL[2] = BG_BMP_BASE(3) | BgSize_B16_256x256;
 
 	REG_BG2PA = 256;
 	REG_BG2PC = 0;
 	REG_BG2PB = 0;
 	REG_BG2PD = 256;
-
-//	BGCTRL[0] = BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1) | BG_32x32;
-//
-//	swiCopy(BGMainTiles,BG_TILE_RAM(1),BGMainTilesLen/2);
-//
-//	swiCopy(BGMainPal,BG_PALETTE,BGMainPalLen/2);
-//
-//	swiCopy(BGMainMap,BG_MAP_RAM(0),BGMainMapLen/2);
-
-
-
-
-
 }
 
 
 void DrawWaveMain(int16_t * main_buffer, int length, int offset , int zoom){
 
+	u16 yellow = ARGB16(1,31,31,0);
+
+	u16 transparent = ARGB16(0,0,0,0);
+
 	for(int x = 0 ; x < 256 ; x ++){
 		for(int y = 0 ; y < 192; y++){
-			BG_BMP_RAM(0)[x + 256 * y] = 0;
+			BG_BMP_RAM(3)[x + 256 * y] = transparent;
 		}
 	}
 
-	u16 yellow = ARGB16(1,31,31,0);
-
-	for(int i = 0; i< length; i++){
+	for(int i = 0; i< length  ; i++){
 
 		int x = (i / (double) 4800) * 256 ;
 
 		int y = ((main_buffer[i] + MAXVALUE) / ((double)2*MAXVALUE)) * 192;
 
-		BG_BMP_RAM(0)[x + 256 * (192-y)] = yellow;
+		int yNext = ((main_buffer[i+1] + MAXVALUE) / ((double)2*MAXVALUE)) * 192;
 
+		if(y - yNext > 0){
+
+			for(int i = 0 ; i<256; i++){
+				BG_BMP_RAM(3)[x + 256 * (192-i)] = yellow;
+			}
+		}
+
+		BG_BMP_RAM(3)[x + 256 * (192-y)] = yellow;
 
 	}
 
