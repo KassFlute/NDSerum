89102ba7d844b5b87b95efd96ba6592a34a23b55
Quentin <quentin.sandoz@epfl.ch>
2024-01-04 19:53:27 +0100

    toujours de probèmes...

--- source/sub_screen.c
+++ source/sub_screen.c
@@ -2,37 +2,38 @@
 
 //Tile #0 (transparent tile)
 u8 tile0[64] = {
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
+    200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200,
+	200, 200, 200, 200, 200, 200, 200, 200
 };
 
 //Tile #1 (black tile)
 u8 tile1[64] = {
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
+    201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201,
+	201, 201, 201, 201, 201, 201, 201, 201
 };
 
 void InitSubScreen(){
 
     VRAM_C_CR = VRAM_ENABLE | VRAM_C_SUB_BG;
-    REG_DISPCNT_SUB = MODE_5_2D | DISPLAY_BG2_ACTIVE | DISPLAY_BG0_ACTIVE;
+    REG_DISPCNT_SUB = MODE_5_2D | DISPLAY_BG0_ACTIVE | DISPLAY_BG2_ACTIVE;
 
     // BG2, Rotoscale
-    BGCTRL_SUB[2] = BG_BMP_BASE(0) | BgSize_B8_256x256;
+    BGCTRL_SUB[2] = BG_BMP_BASE(2) | BgSize_B8_256x256;
+
     // BG0, Tile
-    BGCTRL_SUB[0] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(23) | BG_TILE_BASE(3);
+    BGCTRL_SUB[0] = BG_32x32 | BG_COLOR_256 | BG_MAP_BASE(0) | BG_TILE_BASE(1);
 
     // Affine Marix Transformation
     REG_BG2PA_SUB = 256;
@@ -42,17 +43,28 @@ void InitSubScreen(){
 
     // Copy tiles and palette for the background
     swiCopy(BGSubPal, BG_PALETTE_SUB, BGSubPalLen / 2);
+
     //printf("BGSubPalLen: %d\n", BGSubPalLen);
-    swiCopy(BGSubBitmap, BG_GFX_SUB, BGSubBitmapLen / 2);
+
+    swiCopy(BGSubBitmap, BG_BMP_RAM_SUB(2), BGSubBitmapLen / 2);
+
+    //custom colors for tiles
+    //BG_PALETTE_SUB[200] = ARGB16(0,0,0,0);
+   // BG_PALETTE_SUB[201] = ARGB16(1,31,0,0);
+
+
 
     // Copy tiles for the buttons
-    dmaCopy(tile0, &BG_TILE_RAM_SUB(3)[0], 64);
-    dmaCopy(tile1, &BG_TILE_RAM_SUB(3)[32], 64);
+
+    //POURQUOI CETE LIGNE CASSE TOUT !!!!!!!!!!!! JE VAIS CASSER UN TRUC
+    //dmaCopy(tile0, &BG_TILE_RAM_SUB(1)[0], 64);
+
+    dmaCopy(tile1, &BG_TILE_RAM_SUB(1)[32], 64);
 
     // Generate the map
     for(int i=0; i<32; i+=2) {
         for(int j=0; j<32; j+=2) {
-            BG_MAP_RAM(23)[32*i + j] = 0;
+            BG_MAP_RAM_SUB(0)[32*i + j] = 0;
         }
     }
 }
