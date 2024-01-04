afb8e4a08d71aafea34a2e38079358cea1ef963d
Quentin <quentin.sandoz@epfl.ch>
2024-01-04 15:29:27 +0100

    white noise marche a peut prêt

--- source/white_noise.c
+++ source/white_noise.c
@@ -9,11 +9,16 @@ void WhiteNoiseFill(int16_t* array, int frequency, int* length){
 	 * @param frequency : the frequency of the white noise
 	 * @param length : pointer to write the number of samples written in the array
 	*/
-	int nPeriod = NPeriodFromFrequency(frequency);
-	int sampleNumber = SAMPLERATE / (double) frequency * (nPeriod);
+
+	int nPeriod = NPeriodFromFrequency(frequency); // Quentin
+
+	double samplesPerPeriod = SAMPLERATE/ (double) frequency; // Quentin
+
+	int sampleNumber = samplesPerPeriod * nPeriod ; // Quentin
+
 	*length = sampleNumber;
 
-	for(int i = 0 ; i < sampleNumber ; i++){
+	for(int i = 0 ; i < sampleNumber; i++){
 			array[i] = (short) rand();
 		}
 }
